0249b8529a3a4568b6c5a453946b409521cd3740
owakester <44281235+owakester@users.noreply.github.com>
2022-04-19 22:47:42 -0300

    creando modelo de prueba

--- historias/mihistoria/models.py
+++ historias/mihistoria/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+class MyModelName(models.Model):
+    """
+    Una clase típica definiendo un modelo, derivado desde la clase Model.
+    """
 
-# Create your models here.
+    # Campos
+    my_field_name = models.CharField(max_length=20, help_text="Enter field documentation")
+    ...
+
+    # Metadata
+    class Meta:
+        ordering = ["-my_field_name"]
+
+    # Métodos
+    def get_absolute_url(self):
+         """
+         Devuelve la url para acceder a una instancia particular de MyModelName.
+         """
+         return reverse('model-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        Cadena para representar el objeto MyModelName (en el sitio de Admin, etc.)
+        """
+        return self.field_name
